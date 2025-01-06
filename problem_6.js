8502fbdb2d468d8ec6d42a4bed9cec836bc11d38
Trupti Pimpale <truptipimpale1999@gmail.com>
2025-01-06 14:56:32 +0530

    6th problem added

--- problem_6.js
+++ problem_6.js
@@ -1,4 +1,13 @@
 function longestWord(sentence){
     let word = sentence.split(" ");
     let longest_word = "";
+
+    for(let i of word){
+        if(i.length > longest_word.length){
+            longest_word = i;
+        }
+    }
+    return longest_word;
 }
+
+console.log(longestWord("The quick brown fox jumped over the lazy dog"))
